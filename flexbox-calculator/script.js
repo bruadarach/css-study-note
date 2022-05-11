a6ab142a908d3a8131263cb4273cfb0c8887a8a9
Sujeong Ji <suji.sujeongji@gmail.com>
2022-05-11 23:30:56 +0900

    function updated - operator, decimal

--- flexbox-calculator/script.js
+++ flexbox-calculator/script.js
@@ -12,7 +12,7 @@ function calculate(n1, operator, n2) {
       result = Number(n1) - Number(n2);
     } else if (operator === "*") {
       result = Number(n1) * Number(n2);
-    } else if (operator === "/") {
+    } else if (operator === "÷") {
       result = Number(n1) / Number(n2);
     }
   
@@ -36,34 +36,54 @@ buttons.addEventListener('click', function (event) { // 버튼을 눌렀을 때 
         //     previousKey = 'number';
         // }
         if (action === 'number') {
-            if (display.textContent === '0' && operatorForAdvanced === undefined) {
+            if (display.textContent === '0' || previousKey === 'operator' || previousKey === 'calculate') {
              display.textContent = buttonContent;
-           } else if (display.textContent !== '0' && previousKey === "number") {
-             display.textContent = display.textContent + buttonContent;
-           } else if (operatorForAdvanced !== undefined && previousKey === "operator") {
-             firstNum = display.textContent;
-             display.textContent = buttonContent; 
+           } else { //display.textContent !== '0' && previousKey === "number") {
+            display.textContent = display.textContent + buttonContent;
            } 
+          //  else if (operatorForAdvanced !== undefined && previousKey === "operator") {
+          //    firstNum = display.textContent;
+          //    display.textContent = buttonContent; 
+          //  } 
            previousKey = "number";
          }
 
         if (action === 'operator') {
-            operatorForAdvanced = buttonContent;
+            // operatorForAdvanced = buttonContent;
+            // firstNum = display.textContent;
+            // previousKey = 'operator';
+            if (firstNum && operatorForAdvanced && previousKey !== 'operator' && previousKey !== 'calculate') {
+              display.textContent = calculate(firstNum, operatorForAdvanced, display.textContent);
+            }
             firstNum = display.textContent;
+            operatorForAdvanced = buttonContent;
             previousKey = 'operator';
         }
 
-        if (action === 'decimal') {}
+        if (action === 'decimal') {
+          if (!display.textContent.includes('.')) {
+            display.textContent = display.textContent + '.';
+          } else if (previousKey === 'operator') {
+            display.textContent = '0.';
+          }
+          previousKey = 'decimal';
+        }
 
         if (action === 'calculate') {
-            console.log(buttonContent);
+          if (firstNum) {
             previousNum = display.textContent;
-            display.textContent = calculate(firstNum,operatorForAdvanced,previousNum);
+            let finalResult = calculate(firstNum,operatorForAdvanced,previousNum)
+
+            if (finalResult.includes('.')) {
+              finalResult = Number(finalResult).toFixed(8);
+              display.textContent = String(finalResult);
+            }
+            display.textContent = finalResult;
             previousKey = 'calculate';
+          }  
         }
 
         if (action === 'clear') {
-            console.log(buttonContent);
             display.textContent = '0';
             firstNum = undefined;
             operatorForAdvanced = undefined;
